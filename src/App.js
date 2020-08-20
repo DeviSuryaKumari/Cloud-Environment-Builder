d126c7309625806452d3c954a77058590d54d2ca
Devi Akula <devi.akula@hexagon.com>
2020-08-20 22:39:20 +0530

    Non interactive Home screen

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.scss";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="home">
+      <div className="home__create-estate-card estate-card">
+        <span className="card-text">Create Estate</span>
+      </div>
+      <div className="home__configure-estate-card estate-card">
+        <span className="card-text">Configure Estate</span>
+      </div>
+      <div className="home__handover-estate-card estate-card">
+        <span className="card-text">Handover Estate</span>
+      </div>
     </div>
   );
 }
